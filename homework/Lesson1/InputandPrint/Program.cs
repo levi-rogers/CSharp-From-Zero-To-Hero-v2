4d24cacc725c12b53fbf4524002b74a54c615fc7
Levi C Rogers <levi@blazertech.cloud>
2021-03-21 13:21:20 -0500

    Added Homework Projects

---
+++ homework/Lesson1/InputandPrint/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InputandPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ homework/Problem1/RecipeApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RecipeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
